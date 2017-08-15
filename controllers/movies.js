6dcd8beb81e9976ad29a1410abaeee14438c584b
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-08-14 23:40:48 -0400

    Movie Show page and displaying movie details

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const axios = requires('axios');
+const axios = require('axios');
 
 router
 .get('/', (req, res) => {
   const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
   axios.get(popularMoviesURL)
   .then((api_res) => {
-    console.log(api_res);
-
     res.render('application', {
+      locals: {
+        movies: api_res.data
+      },
       partials: {
         yield: 'views/movies/index.html'
       }
@@ -18,8 +19,23 @@ router
   .catch((err) => {
     return console.log(err);
   });
-
-
+})
+.get('/movies/:id', (req, res) => {
+  const movieURL = `http://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
+  axios.get(movieURL)
+  .then((api_res) => {
+    res.render('application', {
+      locals: {
+        movie: api_res.data
+      },
+      partials: {
+        yield: 'views/movies/show.html'
+      }
+    });
+  })
+  .catch((err) => {
+    return console.log(err);
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express();
 const es6Renderer = require('express-es6-template-engine');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('./env.js');
-}
-
 app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
 app.use(express.static('./assets'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(require('./controllers'));
 
 const port = process.env.PORT || 3000;
 
